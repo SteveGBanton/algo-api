82f4ff28a0839c940ee19ad22a2ac7139c09a53e
Steven G. Banton <steve.g.banton@gmail.com>
2018-04-28 22:00:56 -0400

    formatting update

--- api/dna_compare.go
+++ api/dna_compare.go
@@ -8,51 +8,47 @@ import (
 
 type TwoDNA struct {
 	Title1    string `json:"title1"`
-	Residues1   string `json:"residues1"`
+	Residues1 string `json:"residues1"`
 	Title2    string `json:"title2"`
-	Residues2   string `json:"residues2"`
+	Residues2 string `json:"residues2"`
 }
 
-var sample = map[string]TwoDNA {
+var sample = map[string]TwoDNA{
 	"a": TwoDNA{
-		Title1: "Human insulin sequence",
+		Title1:    "Human insulin sequence",
 		Residues1: "AGCCCTCCAGGACAGGCTGCATCAGAAGAGGCCATCAAGCAGGTCTGTTCCAAGGGCCTTTGCGTCAGGTGGGCTCAGGATTCCAGGGTGGCTGGACCCCAGGCCCCAGCTCTGCAGCAGGGAGGACGTGGCTGGGCTCGTGAAGCATGTGGGGGTGAGCCCAGGGGCCCCAAGGCAGGGCACCTGGCCTTCAGCCTGCCTCAGCCCTGCCTGTCTCCCAGATCACTGTCCTTCTGCCATGGCCCTGTGGATGCGCCTCCTGCCCCTGCTGGCGCTGCTGGCCCTCTGGGGACCTGACCCAGCCGCAGCCTTTGTGAACCAACACCTGTGCGGCTCACACCTGGTGGAAGCTCTCTACCTAGTGTGCGGGGAACGAGGCTTCTTCTACACACCCAAGACCCGCCGGGAGGCAGAGGACCTGCAGGGTGAGCCAACTGCCCATTGCTGCCCCTGGCCGCCCCCAGCCACCCCCTGCTCCTGGCGCTCCCACCCAGCATGGGCAGAAGGGGGCAGGAGGCTGCCACCCAGCAGGGGGTCAGGTGCACTTTTTTAAAAAGAAGTTCTCTTGGTCACGTCCTAAAAGTGACCAGCTCCCTGTGGCCCAGTCAGAATCTCAGCCTGAGGACGGTGTTGGCTTCGGCAGCCCCGAGATACATCAGAGGGTGGGCACGCTCCTCCCTCCACTCGCCCCTCAAACAAATGCCCCGCAGCCCATTTCTCCACCCTCATTTGATGACCGCAGATTCAAGTGTTTTGTTAAGTAAAGTCCTGGGTGACCTGGGGTCACAGGGTGCCCCACGCTGCCTGCCTCTGGGCGAACACCCCATCACGCCCGGAGGAGGGCGTGGCTGCCTGCCTGAGTGGGCCAGACCCCTGTCGCCAGGCCTCACGGCAGCTCCATAGTCAGGAGATGGGGAAGATGCTGGGGACAGGCCCTGGGGAGAAGTACTGGGATCACCTGTTCAGGCTCCCACTGTGACGCTGCCCCGGGGCGGGGGAAGGAGGTGGGACATGTGGGCGTTGGGGCCTGTAGGTCCACACCCAGTGTGGGTGACCCTCCCTCTAACCTGGGTCCAGCCCGGCTGGAGATGGGTGGGAGTGCGACCTAGGGCTGGCGGGCAGGCGGGCACTGTGTCTCCCTGACTGTGTCCTCCTGTGTCCCTCTGCCTCGCCGCTGTTCCGGAACCTGCTCTGCGCGGCACGTCCTGGCAGTGGGGCAGGTGGAGCTGGGCGGGGGCCCTGGTGCAGGCAGCCTGCAGCCCTTGGCCCTGGAGGGGTCCCTGCAGAAGCGTGGCATTGTGGAACAATGCTGTACCAGCATCTGCTCCCTCTACCAGCTGGAGAACTACTGCAACTAGACGCAGCCCGCAGGCAGCCCCACACCCGCCGCCTCCTGCACCGAGAGAGATGGAATAAAGCCCTTGAACCAGC",
-		Title2: "Mouse insulin sequence",
+		Title2:    "Mouse insulin sequence",
 		Residues2: "ACCAGGCAAGTGTTTGGAAACTGCAGCTTCAGCCCCTCTGGCCATCTGCCTACCCACCCCACCTGGAGACCTTAATGGGCCAAACAGCAAAGTCCAGGGGGCAGAGAGGAGGTACTTTGGACTATAAAGCTGGTGGGCATCCAGTAACCCCCAGCCCTTAGTGACCAGCTATAATCAGAGACCATCAGCAAGCAGGTATGTACTCTCCTCTTTGGGCCTGGCTCCCCAGCCAAGACTCCAGCGACTTTAGGGAGAATGTGGGCTCCTCTCTTACATGGATCTTTTGCTAGCCTCAACCCTGCCTATCTTTCAGGTCATTGTTTCAACATGGCCCTGTTGGTGCACTTCCTACCCCTGCTGGCCCTGCTTGCCCTCTGGGAGCCCAAACCCACCCAGGCTTTTGTCAAACAGCATCTTTGTGGTCCCCACCTGGTAGAGGCTCTCTACCTGGTGTGTGGGGAGCGTGGCTTCTTCTACACACCCAAGTCCCGCCGTGAAGTGGAGGACCCACAAGTGGAACAACTGGAGCTGGGAGGAAGCCCCGGGGACCTTCAGACCTTGGCGTTGGAGGTGGCCCGGCAGAAGCGTGGCATTGTGGATCAGTGCTGCACCAGCATCTGCTCCCTCTACCAGCTGGAGAACTACTGCAACTAAGGCCCACCTCGACCCGCCCCACCCCTCTGCAATGAATAAAACTTTTGAATAAGCACCAAAAAAAA",
 	},
 }
 
 func DNACompare(w http.ResponseWriter, r *http.Request) {
 	switch method := r.Method; method {
-		case http.MethodGet:
-			stringA := sample["a"].Residues1
-			stringB := sample["a"].Residues2
-			sequenceLength, tableA := DynamicLCS(stringA, stringB)
-			sequenceA, sequenceB, longestCS := GetSequence(tableA, stringA, stringB)
-			writeJSON(w, sequenceLength)
-			writeJSON(w, sequenceA)
-			writeJSON(w, sequenceB)
-			writeJSON(w, longestCS)
-			writeJSON(w, tableA)
-		case http.MethodPost:
-			body, err := ioutil.ReadAll(r.Body)
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-			}
-			w.Header().Add("Content-Type", "application/json; charset=utf-8")
-			sequences := FromJSON(body)
-			stringA := sequences.Residues1
-			stringB := sequences.Residues2
-			sequenceLength, tableA := DynamicLCS(stringA, stringB)
-			GetSequence(tableA, stringA, stringB)
-			writeJSON(w, sequenceLength)
-			//writeJSON(w, sequenceA)
-			// writeJSON(w, sequenceB)
-			//writeJSON(w, longestCS)
-			// writeJSON(w, tableA)
-		default:
-			w.WriteHeader(http.StatusBadRequest)
-			w.Write([]byte("Unsupported request method."))
+	case http.MethodGet:
+		stringA := sample["a"].Residues1
+		stringB := sample["a"].Residues2
+		sequenceLength, tableA := DynamicLCS(stringA, stringB)
+		sequenceA, sequenceB, longestCS := GetSequence(tableA, stringA, stringB)
+		writeJSON(w, sequenceLength)
+		writeJSON(w, sequenceA)
+		writeJSON(w, sequenceB)
+		writeJSON(w, longestCS)
+		writeJSON(w, tableA)
+	case http.MethodPost:
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+		w.Header().Add("Content-Type", "application/json; charset=utf-8")
+		sequences := FromJSON(body)
+		stringA := sequences.Residues1
+		stringB := sequences.Residues2
+		sequenceLength, tableA := DynamicLCS(stringA, stringB)
+		GetSequence(tableA, stringA, stringB)
+		writeJSON(w, sequenceLength)
+	default:
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Unsupported request method."))
 	}
 }
 
@@ -87,12 +83,12 @@ func DynamicLCS(A string, B string) (int, [][]int) {
 	lengthB := len(B)
 
 	// Make a 2D tables of height lengthA, width lengthB
-	tableA := make([][]int, lengthA + 1)
-	tableB := make([][]int, lengthA + 1)
+	tableA := make([][]int, lengthA+1)
+	tableB := make([][]int, lengthA+1)
 
 	for i := 0; i < (lengthA + 1); i++ {
-		tableA[i] = make([]int, lengthB + 1)
-		tableB[i] = make([]int, lengthB + 1)
+		tableA[i] = make([]int, lengthB+1)
+		tableB[i] = make([]int, lengthB+1)
 	}
 
 	var runesOfA = []rune(A)
@@ -100,20 +96,20 @@ func DynamicLCS(A string, B string) (int, [][]int) {
 
 	for i := 1; i <= lengthA; i++ {
 		for j := 1; j <= lengthB; j++ {
-			if (string(runesOfA[i - 1]) == string(runesOfB[j - 1])) {
-				tableB[i][j] = tableB[i - 1][j - 1] + 1;
-				tableA[i][j] = 2;
-			} else if (tableB[i - 1][j] >= tableB[i][j - 1]) {
-				tableB[i][j] = tableB[i - 1][j];
-				tableA[i][j] = 3;
+			if string(runesOfA[i-1]) == string(runesOfB[j-1]) {
+				tableB[i][j] = tableB[i-1][j-1] + 1
+				tableA[i][j] = 2
+			} else if tableB[i-1][j] >= tableB[i][j-1] {
+				tableB[i][j] = tableB[i-1][j]
+				tableA[i][j] = 3
 			} else {
-				tableB[i][j] = tableB[i][j - 1];
-				tableA[i][j] = 1;
+				tableB[i][j] = tableB[i][j-1]
+				tableA[i][j] = 1
 			}
 		}
 	}
 
-	sequenceLength := tableB[lengthA - 1][lengthB - 1]
+	sequenceLength := tableB[lengthA-1][lengthB-1]
 
 	return sequenceLength, tableA
 }
@@ -126,18 +122,18 @@ func GetSequence(tableA [][]int, A string, B string) ([]string, []string, []stri
 	aCount := len(A)
 
 	for aCount > 0 && bCount > 0 {
-		if (tableA[aCount][bCount] == 2) {
+		if tableA[aCount][bCount] == 2 {
 			aCount -= 1
 			bCount -= 1
 			char := string(A[aCount])
 			sequenceA = append([]string{char}, sequenceA...)
 			sequenceB = append([]string{char}, sequenceB...)
 			longestCS = append([]string{char}, longestCS...)
-		} else if (tableA[aCount][bCount] == 3) {
-			aCount -= 1;
+		} else if tableA[aCount][bCount] == 3 {
+			aCount -= 1
 			sequenceA = append([]string{"*"}, sequenceA...)
 		} else {
-			bCount -= 1;
+			bCount -= 1
 			sequenceB = append([]string{"*"}, sequenceB...)
 		}
 	}
